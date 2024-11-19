5fdab0820716de724be088bd28aae98c96e1be73
Никита <144624133+JRPikhtovnikov@users.noreply.github.com>
2024-11-19 22:21:19 +0300

    Update FULL-HUINYA-YAZIK_PYTHONEONELOVE.cs

--- FULL-HUINYA-YAZIK_PYTHONEONELOVE.cs
+++ FULL-HUINYA-YAZIK_PYTHONEONELOVE.cs
@@ -1,61 +1,59 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using static System.Console;
 using System.Text.Json;
 
-class Task {
-    class City {
-        public string CityName { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
+public class City {
+    public string CityName { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
 
+
+class Program {
     static List<City> OpenFile(string fileName) {
-        try {
-            string json = File.ReadAllText(fileName);
-            return JsonSerializer.Deserialize<List<City>>(json) ?? new List<City>();
-        }
-        catch (Exception ex) {
-            Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
-            return new List<City>();
-        }
+        string json = File.ReadAllText(fileName);
+        return JsonSerializer.Deserialize<List<City>>(json);
     }
 
-    static string CitiesInRound(List<City> library, double xOfShield, double yOfShield, double radOfShield) {
-        var protectedCities = library
-            .Where(city => Math.Pow(city.X - xOfShield, 2) + Math.Pow(city.Y - yOfShield, 2) <= Math.Pow(radOfShield, 2))
-            .Select(city => city.CityName)
-            .ToList();
-
-        return protectedCities.Count != 0
-            ? string.Join(", ", protectedCities)
-            : "Ни один город не защищен.";
+    static List<string> CitiesInRound(List<City> cities, double x, double y, double radius) {
+        return cities.Where(city => Math.Pow(city.X - x, 2) + Math.Pow(city.Y - y, 2) <= Math.Pow(radius, 2))
+                     .Select(city => city.CityName)
+                     .ToList();
     }
 
-    static (double optimalX, double optimalY, double optimalRadius, List<string> maxCities) FindOptimalShieldPosition(List<City> library, double maxRadius) {
+    static List<City> FindNotDefended(List<City> cities, double x, double y, double radius) {
+        var defendedCities = CitiesInRound(cities, x, y, radius);
+        return cities.Where(city => !defendedCities.Contains(city.CityName)).ToList();
+    }
+
+    static bool IsCityDefended(List<City> cities, string cityName, double x, double y, double radius) {
+        var city = cities.FirstOrDefault(c => c.CityName.Equals(cityName, StringComparison.OrdinalIgnoreCase));
+        if (city != null) {
+            return Math.Pow(city.X - x, 2) + Math.Pow(city.Y - y, 2) <= Math.Pow(radius, 2);
+        }
+        return false;
+    }
+
+    static (double x, double y, double radius, List<string> cities) FindOptimalShieldPosition(List<City> cities, double maxRadius) {
         double optimalX = 0, optimalY = 0, optimalRadius = 0;
-        List<string> maxCities = new();
+        List<string> maxCities = new List<string>();
 
-        for (int i = 0; i < library.Count; i++) {
-            for (int j = i + 1; j < library.Count; j++) {
-                var city1 = library[i];
-                var city2 = library[j];
+        for (int i = 0; i < cities.Count; i++) {
+            for (int j = i + 1; j < cities.Count; j++) {
+                var city1 = cities[i];
+                var city2 = cities[j];
 
-                double x1 = city1.X, y1 = city1.Y;
-                double x2 = city2.X, y2 = city2.Y;
-
-                double centerX = (x1 + x2) / 2;
-                double centerY = (y1 + y2) / 2;
-                double radius = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)) / 2;
+                double centerX = (city1.X + city2.X) / 2;
+                double centerY = (city1.Y + city2.Y) / 2;
+                double radius = Math.Sqrt(Math.Pow(city1.X - city2.X, 2) + Math.Pow(city1.Y - city2.Y, 2)) / 2;
 
                 if (radius > maxRadius)
                     continue;
 
-                var protectedCities = library
-                    .Where(city => Math.Pow(city.X - centerX, 2) + Math.Pow(city.Y - centerY, 2) <= Math.Pow(radius, 2))
-                    .Select(city => city.CityName)
-                    .ToList();
+                var protectedCities = CitiesInRound(cities, centerX, centerY, radius);
 
                 if (protectedCities.Count > maxCities.Count) {
                     maxCities = protectedCities;
@@ -66,48 +64,99 @@ class Task {
             }
         }
 
-        foreach (var city in library) {
-            var protectedCities = library
-                .Where(c => Math.Pow(c.X - city.X, 2) + Math.Pow(c.Y - city.Y, 2) <= Math.Pow(maxRadius, 2))
-                .Select(c => c.CityName)
-                .ToList();
-
-            if (protectedCities.Count > maxCities.Count) {
-                maxCities = protectedCities;
-                optimalX = city.X;
-                optimalY = city.Y;
-                optimalRadius = maxRadius;
-            }
-        }
-
         return (optimalX, optimalY, optimalRadius, maxCities);
     }
 
-    static (double xOfShield, double yOfShield, double radOfShield) GetCoords() {
-        try {
-            Console.WriteLine("Введите координаты и радиус щита (x y r): ");
-            string[] inputs = Console.ReadLine()?.Split(' ');
+    static void UserAction(List<City> cities) {
+        var actions = new Dictionary<string, Action>
+        {
+            { "FindNotDefended", () => FindNotDefendedAction(cities) },
+            { "Defended", () => DefendedAction(cities) },
+            { "OptimalDefendPosition", () => OptimalDefendPositionAction(cities) },
+            { "Exit", () => ExitAction() }
+        };
 
-            return (double.Parse(inputs[0]), double.Parse(inputs[1]), double.Parse(inputs[2]));
-        }
-        catch {
-            Console.WriteLine("Ошибка: вводите координаты и радиус в формате 'x y r'.");
-            return (0, 0, 0);
+        while (true) {
+            WriteLine("\nВыберите действие:");
+            foreach (var action in actions.Keys) {
+                WriteLine($"- {action}");
+            }
+
+            string userInput = ReadLine()?.Trim();
+            if (actions.ContainsKey(userInput)) {
+                actions[userInput].Invoke();
+            }
+            else {
+                WriteLine("Неизвестная команда. Попробуйте снова.");
+            }
         }
     }
 
+    static void FindNotDefendedAction(List<City> cities) {
+        Write("Введите координаты и радиус щита (x y r): ");
+        var input = ReadLine()?.Split();
+        if (input != null && input.Length == 3 &&
+            double.TryParse(input[0], out double x) &&
+            double.TryParse(input[1], out double y) &&
+            double.TryParse(input[2], out double radius)) {
+            var notDefendedCities = FindNotDefended(cities, x, y, radius);
+            if (notDefendedCities.Any()) {
+                WriteLine("Незащищенные города:");
+                foreach (var city in notDefendedCities) {
+                    WriteLine($"{city.CityName} ({city.X}, {city.Y})");
+                }
+            }
+            else {
+                WriteLine("Все города защищены.");
+            }
+        }
+        else {
+            WriteLine("Ошибка ввода! Укажите данные в формате x y r.");
+        }
+    }
+
+    static void DefendedAction(List<City> cities) {
+        Write("Введите название города: ");
+        string cityName = ReadLine();
+        Write("Введите координаты и радиус щита (x y r): ");
+        var input = ReadLine()?.Split();
+        if (input != null && input.Length == 3 &&
+            double.TryParse(input[0], out double x) &&
+            double.TryParse(input[1], out double y) &&
+            double.TryParse(input[2], out double radius)) {
+            if (IsCityDefended(cities, cityName, x, y, radius)) {
+                WriteLine($"Город {cityName} защищён.");
+            }
+            else {
+                WriteLine($"Город {cityName} не защищён.");
+            }
+        }
+        else {
+            WriteLine("Ошибка ввода! Укажите данные в формате x y r.");
+        }
+    }
+
+    static void OptimalDefendPositionAction(List<City> cities) {
+        Write("Введите максимальный радиус щита: ");
+        if (double.TryParse(ReadLine(), out double maxRadius)) {
+            var (x, y, radius, defendedCities) = FindOptimalShieldPosition(cities, maxRadius);
+            WriteLine("Оптимальное место для щита:\n" +
+            $"Координаты: ({x}, {y})\n" +
+            $"Радиус: {radius}OptimalDefendPosition\n" +
+            $"Защищенные города: {string.Join(", ", defendedCities)}");
+        }
+        else {
+            WriteLine("Ошибка ввода! Укажите допустимое значение радиуса.");
+        }
+    }
+
+    static void ExitAction() {
+        WriteLine("Выход из программы.");
+        Environment.Exit(0);
+    }
+
     static void Main() {
-        var library = OpenFile("../../../cities.json");
-
-        var userShieldCoords = GetCoords();
-        Console.WriteLine($"Города, попадающие под щит с указанными данными:\n" +
-                          $"{CitiesInRound(library, userShieldCoords.xOfShield, userShieldCoords.yOfShield, userShieldCoords.radOfShield)}\n");
-
-        var (optimalX, optimalY, optimalRadius, maxCities) = FindOptimalShieldPosition(library, 100);
-
-        Console.WriteLine("Оптимальное место для щита: \n" +
-        $"Координаты: ({optimalX}, {optimalY})\n" +
-        $"Радиус: {optimalRadius}\n" +
-        $"Защищенные города: {string.Join(" ", maxCities)}");
+        var cities = OpenFile("../../../cities.json");
+        UserAction(cities);
     }
 }
